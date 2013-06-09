3b9d58f3855422de0f981917ab2945eb5ef9d641
Thomas Gebert <thomasgebert@gmail.com>
2013-06-09 14:40:27 -0500

    Finish the examine and kill function

--- Cell.scala
+++ Cell.scala
@@ -1,11 +1,25 @@
 class Cell {
-  var _dead:Boolean = true
-  var _markedForDead:Boolean = false
+  var _alive:Boolean = false
+  var _markedForDead:Boolean = true
+  var _markedForLiving:Boolean = false
 
-  def dead = _dead
-  def dead_=(d:Boolean):Unit = _dead = d
+  def alive = _alive
+  def alive_=(d:Boolean):Unit = _alive = d
 
-  def markedForDead = _markedForDead
-  def markedForDead_= (m:Boolean):Unit = _markedForDead = m
+  // def markedForDead = _markedForDead
+  // def markedForDead_= (m:Boolean):Unit = _markedForDead = m
   
+
+  def markedForLiving = _markedForLiving
+
+  def markedForLiving_= (l:Boolean):Unit = _markedForLiving = l
+  
+
+  def kill:Unit = {
+    if(markedForLiving){
+      alive = true
+    } else {
+      alive = false
+    }
+  }
 }
--- Grid.scala
+++ Grid.scala
@@ -1,12 +1,37 @@
 class Grid{
-  var arrayOfCells = Array.ofDim[Cell](100, 100)
+  private var arrayOfCells = Array.ofDim[Cell](100, 100)
   
   def examine = {
+
     for(i<- 1 to 100){
       for(j<-1 to 100){
-	arrayOfCells(i)(j)
+	var countArray = Array.ofDim[Boolean](8) //One for each adjacent and diagonal cell
+
+  	countArray(1) = arrayOfCells(i-1)(j-1).alive
+	countArray(2) = arrayOfCells(i)(j-1).alive
+	countArray(3) = arrayOfCells(i+1)(j-1).alive
+	countArray(4) = arrayOfCells(i+1)(j).alive
+	countArray(5) = arrayOfCells(i-1)(j).alive
+	countArray(6) = arrayOfCells(i)(j+1).alive
+	countArray(7) = arrayOfCells(i-1)(j+1).alive
+	countArray(8) = arrayOfCells(i+1)(j+1).alive
+	
+	if(countArray.count(_ == true) == 2 || countArray.count(_ == true) == 3){
+	  arrayOfCells(i)(j).markedForLiving = true
+	}
+	else {
+	  arrayOfCells(i)(j).markedForLiving = false
+	}
+	
       }
     }
   }
-  
+
+  def kill = {
+    // def checkToKill(myObject:Cell) = {
+    //   if (myObject.markedForLiving == false) myObject.alive = false
+    // }
+    
+    arrayOfCells.par.map(i => i.par.map(j=>j.kill))
+  }
 }
