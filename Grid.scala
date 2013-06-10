18a8e74ecc719d075b32b5461e5534ae6fb10e73
Thomas Gebert <thomasgebert@gmail.com>
2013-06-09 20:49:35 -0500

    We now have a working system.  We don't have it working right on the edge conditoins, but the gliders do seem to work.

--- Grid.scala
+++ Grid.scala
@@ -1,58 +1,67 @@
 class Grid{
-  var arrayOfCells = Array.fill(100,100){new Cell}
+  var arrayOfCells = Array.fill(50,50){new Cell}
+  arrayOfCells (19)(20).alive = true
   arrayOfCells (20)(20).alive = true
   arrayOfCells (21)(20).alive = true
-  arrayOfCells (20)(21).alive = true
-  arrayOfCells (21)(21).alive = true
+  arrayOfCells (21)(19).alive = true
+  arrayOfCells (20)(18).alive = true
+  //Coordinates of a glider. Need to make a text parser, I think
+
   def display = {
     for(i<-arrayOfCells) {
       for(j<-i) {
        
     	if(j.alive){
-    	  print("x")
+    	  print("X")
     	}
     	else {
-    	  print("o")
+    	  print(" ")
     	}
       }
      println("")
     }
+    println("")
     this.examine
   }
 
   def examine = {
     var countArray = Array.fill(8){false} //One for each adjacent and diagonal cell
  
-    for(i<- 0 to 99){
-      for(j<- 0 to 99) {
+    for(i<- 0 to 49){
+      for(j<- 0 to 49) {
 
 	
    	if(i>0 && j>0){
 	  countArray(0) = arrayOfCells(i-1)(j-1).alive
 	}
-	if(i<99 && j<99){
+	if(i<49 && j<49){
     	  countArray(1) = arrayOfCells(i+1)(j+1).alive
 	}
 	if(j>0){
     	  countArray(2) = arrayOfCells(i)(j-1).alive
 	}
-	if(j>0 && i<99){
+	if(j>0 && i<49){
     	  countArray(3) = arrayOfCells(i+1)(j-1).alive
 	}
-	if(i<99){
+	if(i<49){
     	  countArray(4) = arrayOfCells(i+1)(j).alive
 	}
    	if(i>0){
     	  countArray(5) = arrayOfCells(i-1)(j).alive
 	}
-	if(j<99){
+	if(j<49){
     	  countArray(6) = arrayOfCells(i)(j+1).alive
 	}
-	if(i>0 && j<99){
+	if(i>0 && j<49){
     	  countArray(7) = arrayOfCells(i-1)(j+1).alive
 	}
 
-    	if(countArray.count(_ == true) == 2 || countArray.count(_ == true) == 3){
+	var theCount = countArray.count(_ == true)
+	if (!arrayOfCells(i)(j).alive && theCount==3){
+    	  arrayOfCells(i)(j).markedForLiving = true
+	}
+
+    	else if(arrayOfCells(i)(j).alive && (theCount == 2 || theCount == 3)){
 
     	  arrayOfCells(i)(j).markedForLiving = true
     	}
--- main.scala
+++ main.scala
@@ -4,6 +4,7 @@ object mainGame {
    
     while(true){
       Runtime.getRuntime().exec("clear");
+      //I don't know of a reliable way to do this.....You're going to need a posix system to run it. 
       myGrid.display
 
     }
